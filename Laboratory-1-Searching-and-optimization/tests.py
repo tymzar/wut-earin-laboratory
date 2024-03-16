eb615ed4206cb5dfb7a8441371bf94728a84011e
barpec12 <kontakt@barpec12.pl>
2024-03-16 19:08:28 +0100

    Add test for reachable

--- Laboratory-1-Searching-and-optimization/tests.py
+++ Laboratory-1-Searching-and-optimization/tests.py
@@ -14,5 +14,17 @@ class TestMaze(unittest.TestCase):
         self.assertEqual([], bfs_final_path)
         self.assertEqual([], dfs_final_path)
 
+    def test_reachable(self):
+        maze = [[0, 1, 0], [0, 1, 0], [0, 0, 0], [0, 1, 1]]
+        start_position = (0, 0)
+        finish_position = (1, 2)
+        bfs_maze = Search(maze, start_position, finish_position, "bfs")
+        bfs_final_path, _, _  = bfs_maze.search()
+        dfs_maze = Search(maze, start_position, finish_position, "dfs")
+        dfs_final_path, _, _  = dfs_maze.search()
+
+        self.assertNotEqual(0, len(bfs_final_path))
+        self.assertNotEqual(0, len(dfs_final_path))
+
 if __name__ == '__main__':
     unittest.main()
