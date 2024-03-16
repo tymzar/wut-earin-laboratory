cc165fcf1ed42ced5f7f67982fa6b81533044dfe
barpec12 <kontakt@barpec12.pl>
2024-03-16 18:57:18 +0100

    Add dfs to test

--- Laboratory-1-Searching-and-optimization/tests.py
+++ Laboratory-1-Searching-and-optimization/tests.py
@@ -6,9 +6,13 @@ class TestMaze(unittest.TestCase):
         maze = [[0, 1, 0], [1, 1, 0], [0, 0, 0], [0, 1, 1]]
         start_position = (0, 0)
         finish_position = (1, 2)
-        current_maze = Search(maze, start_position, finish_position, "bfs")
-        step_number, history, last_node = current_maze.search()
-        self.assertEqual(-1, step_number)
+        bfs_maze = Search(maze, start_position, finish_position, "bfs")
+        bfs_step, _, _  = bfs_maze.search()
+        dfs_maze = Search(maze, start_position, finish_position, "dfs")
+        dfs_step, _, _  = dfs_maze.search()
+
+        self.assertEqual(-1, bfs_step)
+        self.assertEqual(-1, dfs_step)
 
 if __name__ == '__main__':
     unittest.main()
