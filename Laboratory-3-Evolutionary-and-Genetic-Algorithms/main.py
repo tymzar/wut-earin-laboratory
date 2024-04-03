d0d083d454f5a3f6460f63c0c829354d0a04f8b2
barpec12 <kontakt@barpec12.pl>
2024-04-03 22:00:07 +0200

    Use log scale, add x axis labels

--- Laboratory-3-Evolutionary-and-Genetic-Algorithms/main.py
+++ Laboratory-3-Evolutionary-and-Genetic-Algorithms/main.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     # TODO Experiment 1...
     ga = GeneticAlgorithm(
         population_size=1000,
-        mutation_rate=0.5,
+        mutation_rate=0.2,
         mutation_strength=10,
         crossover_rate=0.3,
-        num_generations=100,
+        num_generations=30,
         tournament_size=30
     )
 
@@ -23,16 +23,21 @@ if __name__ == "__main__":
 
     axis[0, 0].plot(range(0, ga.num_generations), list(map(lambda sol: sol[0], best_solutions)))
     axis[0, 0].set_title("Best solutions X")
+    axis[0, 0].set_xlabel("Generation")
 
     axis[0, 1].plot(range(0, ga.num_generations), list(map(lambda sol: sol[1], best_solutions)))
     axis[0, 1].set_title("Best solutions Y")
+    axis[0, 1].set_xlabel("Generation")
 
     axis[1, 0].plot(range(0, ga.num_generations), best_fitness_values)
     axis[1, 0].set_title("Best fitness value")
+    axis[1, 0].set_yscale("log")
+    axis[1, 0].set_xlabel("Generation")
 
     axis[1, 1].plot(range(0, ga.num_generations), average_fitness_values)
     axis[1, 1].set_title("Average fitness value")
-
+    axis[1, 1].set_yscale("log")
+    axis[1, 1].set_xlabel("Generation")
     plt.show()
 
     # print(f'Best solutions: ${best_solutions}. Best fitness: ${best_fitness_values} average fitness_values: ${average_fitness_values}')
