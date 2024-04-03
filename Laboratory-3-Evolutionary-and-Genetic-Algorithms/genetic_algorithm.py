6a3e84d55e3e30e8343ca4a8ec3ed0a2e6600ce5
barpec12 <kontakt@barpec12.pl>
2024-04-03 18:04:14 +0200

    Initialize population

--- Laboratory-3-Evolutionary-and-Genetic-Algorithms/genetic_algorithm.py
+++ Laboratory-3-Evolutionary-and-Genetic-Algorithms/genetic_algorithm.py
@@ -2,7 +2,8 @@ import random
 from numpy import random as numpy_random, nan
 from pandas import isna
 from typing import Tuple
-
+from functions import init_ranges
+from functions import rosenbrock_2d
 
 def set_seed(seed: int) -> None:
     # Set fixed random seed to make the results reproducible
@@ -29,45 +30,8 @@ class GeneticAlgorithm:
         self.crossover_rate = crossover_rate
         self.num_generations = num_generations
 
-    def decode_binary_to_decimal(
-        self, value_range: ValueRange, number_of_bits: int, individual: BinaryIndividual
-    ) -> int:
-
-        if len(individual) != number_of_bits:
-            raise ValueError(
-                f"Individual length ({len(individual)}) does not match the number of bits ({number_of_bits})"
-            )
-        if len(value_range) != 2:
-            raise ValueError("Value range must contain two values")
-
-        lower_bound, upper_bound = value_range
-
-        if lower_bound >= upper_bound:
-            raise ValueError("Lower bound must be smaller than upper bound")
-
-        decoded_value = nan
-        largest_value = 2**number_of_bits - 1
-
-        binary_characters = "".join(str(bit) for bit in individual)
-        integer_value = int(binary_characters, 2)
-
-        decoded_value = lower_bound + (integer_value / largest_value) * (
-            upper_bound - lower_bound
-        )
-
-        if decoded_value < lower_bound or decoded_value > upper_bound:
-            raise ValueError(
-                f"Decoded value ({decoded_value}) is out of the value range ({value_range})"
-            )
-
-        if isna(decoded_value):
-            raise ValueError("Decoded value is not a number")
-
-        return decoded_value
-
-    def initialize_population(self) -> ...:
-        # TODO Initialize the population and return the result
-        ...
+    def initialize_population(self):
+        return [(random.uniform(*init_ranges[rosenbrock_2d][0]), random.uniform(*init_ranges[rosenbrock_2d][1])) for _ in range(self.population_size)]
 
     def evaluate_population(self, population) -> ...:
         # TODO Evaluate the fitness of the population and return the values for each individual in the population
--- Laboratory-3-Evolutionary-and-Genetic-Algorithms/main.py
+++ Laboratory-3-Evolutionary-and-Genetic-Algorithms/main.py
@@ -3,10 +3,11 @@ from genetic_algorithm import GeneticAlgorithm
 if __name__ == "__main__":
     # TODO Experiment 1...
     ga = GeneticAlgorithm(
-        population_size=...,
-        mutation_rate=...,
-        mutation_strength=...,
-        crossover_rate=...,
-        num_generations=...,
+        population_size=10,
+        mutation_rate=0,
+        mutation_strength=0,
+        crossover_rate=0,
+        num_generations=1,
     )
-    best_solutions, best_fitness_values, average_fitness_values = ga.evolve(seed=...)
+    print(str(ga.initialize_population()))
+    # best_solutions, best_fitness_values, average_fitness_values = ga.evolve(seed=...)
