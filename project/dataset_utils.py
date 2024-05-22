565fff26cbbc76e083900a27e9b04da165cd8ac9
barpec12 <kontakt@barpec12.pl>
2024-05-22 12:43:13 +0200

    Move downloading to external method

--- project/dataset_utils.py
+++ project/dataset_utils.py
@@ -4,7 +4,7 @@ import pandas as pd
 import kaggle
 
 
-def load_track_details() -> DataFrame:
+def download_10_m_tracks_dataset():
     if not os.path.isdir("datasets/10-m-tracks"):
         kaggle.api.authenticate()
         kaggle.api.dataset_download_files(
@@ -13,31 +13,18 @@ def load_track_details() -> DataFrame:
             unzip=True,
         )
 
+def load_track_details() -> DataFrame:
+    download_10_m_tracks_dataset()
     df = pd.read_csv("datasets/10-m-tracks/bp_track.csv")
     return df
 
-
 def load_artist_release() -> DataFrame:
-    if not os.path.isdir("datasets/10-m-tracks"):
-        kaggle.api.authenticate()
-        kaggle.api.dataset_download_files(
-            "mcfurland/10-m-beatport-tracks-spotify-audio-features",
-            path="datasets/10-m-tracks",
-            unzip=True,
-        )
-
+    download_10_m_tracks_dataset()
     df = pd.read_csv("datasets/10-m-tracks/bp_artist_release.csv")
     return df
 
 
 def load_artist_details() -> DataFrame:
-    if not os.path.isdir("datasets/10-m-tracks"):
-        kaggle.api.authenticate()
-        kaggle.api.dataset_download_files(
-            "mcfurland/10-m-beatport-tracks-spotify-audio-features",
-            path="datasets/10-m-tracks",
-            unzip=True,
-        )
-
+    download_10_m_tracks_dataset()
     df = pd.read_csv("datasets/10-m-tracks/bp_artist.csv")
     return df
